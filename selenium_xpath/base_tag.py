40c48de818524012a9247a333b1a031230e4b037
Nick Beaird <nicklasbeaird@gmail.com>
2020-03-22 23:12:50 -0700

    Add Flake8 to pre-commit

--- selenium_xpath/base_tag.py
+++ selenium_xpath/base_tag.py
@@ -1,9 +1,5 @@
-from typing import Optional, Dict
-
-
 class BaseTag:
-
-    def __init__(self, xpath: str = '//*', **kwargs):
+    def __init__(self, xpath: str = "//*", **kwargs):
         self.user_input = kwargs
         self.xpath = xpath
 
@@ -20,27 +16,29 @@ class BaseTag:
         Set the xpath value of the string as value
         """
         if not isinstance(xpath, str):
-            raise ValueError('xpath must be of type str')
+            raise ValueError("xpath must be of type str")
 
         for key in self.user_input.keys():
-            if key.startswith('html_'):
+            if key.startswith("html_"):
                 new_value = key[5:]
-                xpath += f'[@{new_value}={self.user_input[key]}]'
+                xpath += f"[@{new_value}={self.user_input[key]}]"
             else:
-                xpath += f'[@{key}={self.user_input[key]}]'
+                xpath += f"[@{key}={self.user_input[key]}]"
         self._xpath = xpath
 
     def __repr__(self):
-        return f'{self.__class__.__name__}({str(self.xpath)})'
+        return f"{self.__class__.__name__}({str(self.xpath)})"
 
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
     def get_tags():
-        a = BaseTag('//br', html_class='new_class', html_id="old_id", time_attr="newtest")
-        print('__repr__: ', a)
-        print('a.xpath: ', a.xpath)
-        print('a: ', a)
+        a = BaseTag(
+            "//br", html_class="new_class", html_id="old_id", time_attr="newtest"
+        )
+        print("__repr__: ", a)
+        print("a.xpath: ", a.xpath)
+        print("a: ", a)
         b = BaseTag()
         print(b)
 
