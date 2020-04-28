07b210ea83ee885a44b7c40d722dd98badc75291
Nick Beaird <nicklasbeaird@gmail.com>
2020-04-27 22:11:47 -0700

    Remove type hint that changed imported type
    GH-21

--- pom_elements/base_element.py
+++ pom_elements/base_element.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Optional
+from typing import Optional
 
 from pom_elements.harness import Harness
 from selenium import webdriver
@@ -191,7 +191,7 @@ class BaseElement(ABC):
             self.web_element,
         )
 
-    def get_property(self, property: str) -> Any:
+    def get_property(self, property: str):
         """Get the property of the element.
 
         Args:
