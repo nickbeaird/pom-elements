3150c497b5e935c6e9db8ab4bf8a538a5fd58d0b
Nick Beaird <nicklasbeaird@gmail.com>
2020-05-03 01:33:27 -0700

    Add html tags for h1-h6
    GH-18

---
+++ pom_elements/html/__init__.py
@@ -0,0 +1,15 @@
+"""The Python package `pom-elements.html` module is for interacting with html Elements.
+
+`pom-elements.html` extends the XPathElement class to improve the `pom-elements`
+experience writing tests for DOM elements.
+"""
+from .elements import H1, H2, H3, H4, H5, H6
+
+__all__ = [
+    "H1",
+    "H2",
+    "H3",
+    "H4",
+    "H5",
+    "H6",
+]
